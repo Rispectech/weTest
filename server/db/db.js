2ef191f8a11d2045802c4f128564ae0071cbe86a
Rispectech <ue208085.rishabhverma.it@gmail.com>
2024-09-24 17:48:24 +0530

    deply fix

--- server/db/db.js
+++ server/db/db.js
@@ -9,9 +9,11 @@ const sequelize = new Sequelize(
     dialect: "postgres",
     logging: false,
     password: process.env.DB_PORT,
-    ssl: {
-      require: true, // Set to true if connecting to a remote database with SSL
-      rejectUnauthorized: false, // You can set this to true if you have a certificate for verification
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
   }
 );
