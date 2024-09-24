5225ffb8b7708e8e2d3a05a75e2d91fd523a949a
Rispectech <ue208085.rishabhverma.it@gmail.com>
2024-09-24 17:39:43 +0530

    deply backend

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,19 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("weTest", "root", "rishabh132001", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: false,
+    password: process.env.DB_PORT,
+    ssl: {
+      require: true, // Set to true if connecting to a remote database with SSL
+      rejectUnauthorized: false, // You can set this to true if you have a certificate for verification
+    },
+  }
+);
 
 module.exports = sequelize;
